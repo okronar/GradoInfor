54119ec5908c0591f56e7401a861fe825cd186e9
okronar <okronar@gmail.com>
2023-12-04 16:06:37 +0100

    .

---
+++ Interfaces-trabajos/ConsoleBurguer/MyBurgerLib/Pedido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger
+{
+    internal class Pedido
+    {
+        public List<Item> pedido;
+        public DateTime date;
+
+        public Pedido(){
+            pedido = new List<Item>();
+            date = DateTime.Now;
+        }
+
+        public void addItem(Item it)
+        {
+            pedido.Add(it);
+        }
+
+        public double getTotal()
+        {
+            double total = 0;
+            foreach (Item it in pedido)
+            {
+                total += it.CalculatePrice();
+            }
+            return total;
+        }
+        public List<Item> getItems()
+        {
+            return pedido;
+        }
+        public DateTime getDate()
+        {
+            return date;
+        }
+    }
+}
