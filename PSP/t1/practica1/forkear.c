737cdf05ee4ee14ea17c21aa4c3d4468cb8f45c1
okronar <okronar@gmail.com>
2023-12-12 15:26:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- PSP/t1/practica1/forkear.c
+++ PSP/t1/practica1/forkear.c
@@ -5,19 +5,17 @@
 int main(int argc, char *argv[])
 {
     pid_t pid;
- 
+    
     if ( (pid=fork()) == 0 )
     { /* hijo */
         printf("Soy el hijo (%d, hijo de %d)\n", getpid(), getppid());
         return 0;
     }
     else
-     if((pid=fork())>0){ /* padre */
+     { /* padre */
+        
         printf("Soy el padre (%d, hijo de %d)\n", getpid(), getppid());
         return 0;
-    }else {
-
-        printf("Error al crear un hijo, se acabó")
     }
  
     return 0;
