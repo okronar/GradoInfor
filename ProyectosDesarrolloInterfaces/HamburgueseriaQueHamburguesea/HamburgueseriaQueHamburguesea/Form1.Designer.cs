1fc7131544c53477e07dd6bb659f8ccd7bb142f3
okronar <okronar@gmail.com>
2024-01-11 19:17:52 +0100

    .

--- ProyectosDesarrolloInterfaces/HamburgueseriaQueHamburguesea/HamburgueseriaQueHamburguesea/Form1.Designer.cs
+++ ProyectosDesarrolloInterfaces/HamburgueseriaQueHamburguesea/HamburgueseriaQueHamburguesea/Form1.Designer.cs
@@ -30,6 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             Titulo = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // Titulo
@@ -41,15 +50,75 @@
             Titulo.Size = new Size(0, 15);
             Titulo.TabIndex = 0;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(642, 538);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, -2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(959, 133);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(643, 128);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(319, 96);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.Location = new Point(643, 221);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(319, 315);
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Cursor = Cursors.No;
+            button1.ForeColor = Color.DarkOrange;
+            button1.Location = new Point(731, 291);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 56);
+            button1.TabIndex = 5;
+            button1.Text = "Menú";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.InactiveBorder;
             ClientSize = new Size(962, 532);
+            Controls.Add(button1);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Controls.Add(Titulo);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +126,10 @@
         #endregion
 
         private Label Titulo;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Button button1;
     }
 }
--- ProyectosDesarrolloInterfaces/HamburgueseriaQueHamburguesea/HamburgueseriaQueHamburguesea/obj/Debug/net6.0-windows/HamburgueseriaQueHamburguesea.AssemblyInfo.cs
+++ ProyectosDesarrolloInterfaces/HamburgueseriaQueHamburguesea/HamburgueseriaQueHamburguesea/obj/Debug/net6.0-windows/HamburgueseriaQueHamburguesea.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HamburgueseriaQueHamburguesea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f551b7f4905dbe4b9092c4c0c5513128a0ec105")]
 [assembly: System.Reflection.AssemblyProductAttribute("HamburgueseriaQueHamburguesea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HamburgueseriaQueHamburguesea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
